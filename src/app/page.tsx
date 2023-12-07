853b9cb152ecedfa8ccdd8dce3370bece09ce491
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-12-06 21:46:37 -0500

    import odometer form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
+import OdometerForm from '@/components/OdemeterForm'
 import Image from 'next/image'
 
 export default function Home() {
   return (
-    <div className='flex flex-col justify-center items-center h-screen text-4xl font-bold'>
-      <h1>Home Page</h1>
+    <div className='flex flex-col justify-center items-center  text-4xl font-bold'>
+      <OdometerForm />
     </div>
   )
 }
