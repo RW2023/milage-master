a3dbae06500577a3f5a6e82a7ce6d028848961e2
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-12-06 21:50:40 -0500

    pull in about from other project will edit later

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,5 +1,5 @@
 'use client';
-
+import Heading from '@/components/ui/Heading'; 
 import React from 'react';
 
 export default function About() {
@@ -7,9 +7,10 @@ export default function About() {
     <div className="bg-base-200 min-h-screen">
       <div className="container mx-auto p-6">
         {/* Main Heading */}
-        <h1 className="text-5xl font-bold text-center mb-6">
-          About Inspirational Voices
-        </h1>
+        <Heading
+          title="About Us"
+          iconClass="fas fa-truck"
+        />
 
         {/* Our Journey Section */}
         <div className="card bg-base-100 shadow-xl mb-4">
@@ -52,22 +53,22 @@ export default function About() {
               Thus, Inspirational Voices was born - a digital tribute to black
               thought leaders. Each time you click the quote generator button,
               not only does a random, impactful quote appear, but so does the
-                        image of the leader who said it. This integration of visual and
-                        textual elements aims to create a more immersive and meaningful
-                        experience.
-                    </p>
-                </div>
-            </div>
+              image of the leader who said it. This integration of visual and
+              textual elements aims to create a more immersive and meaningful
+              experience.
+            </p>
+          </div>
+        </div>
 
-            {/* More Than Just an App Section */}
-            <div className="card bg-base-100 shadow-xl mb-4">
-                <div className="card-body">
-                    <h2 className="card-title text-4xl">More Than Just an App</h2>
-                    <p>
-                        This app is a blend of technology and history, designed to
-                        educate, motivate, and resonate with its users. It&apos;s a testament
-              to how a simple practice in coding can evolve into something
-              impactful and significant.
+        {/* More Than Just an App Section */}
+        <div className="card bg-base-100 shadow-xl mb-4">
+          <div className="card-body">
+            <h2 className="card-title text-4xl">More Than Just an App</h2>
+            <p>
+              This app is a blend of technology and history, designed to
+              educate, motivate, and resonate with its users. It&apos;s a
+              testament to how a simple practice in coding can evolve into
+              something impactful and significant.
             </p>
           </div>
         </div>
