62c74337fc625915ebc4535fd704f049f5d28784
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-12-07 12:26:59 -0500

    enhance error handling to reduce typscript warning and errors

--- src/components/OdemeterForm.tsx
+++ src/components/OdemeterForm.tsx
@@ -1,37 +1,80 @@
-//src/components/OdemeterForm.tsx
+//src/components/OdometerForm.tsx
 'use client';
+
 import React, { useState } from 'react';
-import Heading from '@/components/ui/Heading'; // Adjust the import path as needed
-import SubHeading from '@/components/ui/SubHeading'; // Adjust the import path as needed
+import Heading from '@/components/ui/Heading';
+import SubHeading from '@/components/ui/SubHeading';
 
 const OdometerForm = () => {
   const [startReading, setStartReading] = useState('');
   const [endReading, setEndReading] = useState('');
-  const [error, setError] = useState('');
-  const [tripId, setTripId] = useState('');
+  const [feedback, setFeedback] = useState({ message: '', isError: false });
+
+  const handleSubmit = async (start: string, end: string, isStart: boolean) => {
+    try {
+      const response = await fetch('/api/addReading', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          startReading: isStart ? start : undefined,
+          endReading: isStart ? undefined : end,
+        }),
+      });
+
+      if (!response.ok) {
+        const errorText = await response.text();
+        throw new Error(errorText || 'Server responded with an error');
+      }
+
+      setFeedback({ message: 'Reading saved successfully!', isError: false });
+      setStartReading('');
+      setEndReading('');
+    } catch (error) {
+      if (error instanceof Error) {
+        setFeedback({
+          message: error.message || 'Failed to save reading. Please try again.',
+          isError: true,
+        });
+      }
+    }
+  };
 
   const handleStartSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    // Add logic to store the start reading
+    handleSubmit(startReading, '', true);
   };
 
   const handleEndSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    // Add logic to store the end reading and calculate the distance
+    handleSubmit('', endReading, false);
   };
 
   return (
-    <div className="container mx-auto p-4 md:w-3/4 lg:w-1/2">
+    <div className="container mx-auto p-4">
       <Heading title="Odometer Tracking" iconClass="fas fa-tachometer-alt" />
 
+      {/* Feedback Message */}
+      {feedback.message && (
+        <div
+          className={`alert ${
+            feedback.isError ? 'alert-error' : 'alert-success'
+          }`}
+        >
+          <div className="flex-1">
+            <label>{feedback.message}</label>
+          </div>
+        </div>
+      )}
+
       {/* Start Reading Form */}
-      <div className="card bg-base-100 shadow-xl mb-6">
-        <div className="card-body border">
+      <div className="card bg-base-100 shadow-xl my-6">
+        <div className="card-body">
           <SubHeading
             title="Enter Start Reading"
             iconClass="fas fa-pencil-alt"
           />
-          {error && <p className="text-red-500">{error}</p>}
           <form onSubmit={handleStartSubmit} className="flex flex-col gap-4">
             <div className="form-control">
               <label className="label">
@@ -54,8 +97,8 @@ const OdometerForm = () => {
       </div>
 
       {/* End Reading Form */}
-      <div className="card bg-base-100 shadow-xl">
-        <div className="card-body border">
+      <div className="card bg-base-100 shadow-xl my-6">
+        <div className="card-body">
           <SubHeading title="Enter End Reading" iconClass="fas fa-pencil-alt" />
           <form onSubmit={handleEndSubmit} className="flex flex-col gap-4">
             <div className="form-control">
