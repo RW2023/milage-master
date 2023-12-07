ca7913dd7e52b46add3ecc0f8ab6587f615b0fe1
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-12-06 22:03:34 -0500

    update form logic

--- src/components/OdemeterForm.tsx
+++ src/components/OdemeterForm.tsx
@@ -1,80 +1,81 @@
 'use client';
-// OdometerForm.tsx
 import React, { useState } from 'react';
-import Heading from '@/components/ui/Heading'; 
-import SubHeading from '@/components/ui/SubHeading'; 
+import Heading from '@/components/ui/Heading'; // Adjust the import path as needed
+import SubHeading from '@/components/ui/SubHeading'; // Adjust the import path as needed
 
 const OdometerForm = () => {
   const [startReading, setStartReading] = useState('');
   const [endReading, setEndReading] = useState('');
   const [error, setError] = useState('');
+  const [tripId, setTripId] = useState('');
 
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+  const handleStartSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
+    // Add logic to store the start reading
+  };
 
-    setError(''); // Reset any existing error messages
-
-    const start = parseFloat(startReading);
-    const end = parseFloat(endReading);
-
-    if (isNaN(start) || isNaN(end)) {
-      setError('Please enter valid numbers for both readings.');
-      return;
-    }
-
-    if (end <= start) {
-      setError('End reading must be greater than start reading.');
-      return;
-    }
-
-    const distance = end - start;
-    console.log({ startReading: start, endReading: end, distance });
-
-    // Reset fields after successful submission
-    setStartReading('');
-    setEndReading('');
+  const handleEndSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    // Add logic to store the end reading and calculate the distance
   };
 
   return (
     <div className="container mx-auto p-4">
       <Heading title="Odometer Tracking" iconClass="fas fa-tachometer-alt" />
-      <SubHeading title="Enter Start and End Readings" iconClass="fas fa-pencil-alt" />
 
-      <form onSubmit={handleSubmit} className="flex flex-col gap-4">
-        {error && <p className="text-red-500">{error}</p>}
-
-        <div className="form-control">
-          <label className="label">
-            <span className="label-text">Start Reading</span>
-          </label>
-          <input
-            type="number"
-            value={startReading}
-            onChange={(e) => setStartReading(e.target.value)}
-            placeholder="Start Odometer Reading"
-            className="input input-bordered"
-            required
+      {/* Start Reading Form */}
+      <div className="card bg-base-100 shadow-xl mb-6">
+        <div className="card-body">
+          <SubHeading
+            title="Enter Start Reading"
+            iconClass="fas fa-pencil-alt"
           />
+          {error && <p className="text-red-500">{error}</p>}
+          <form onSubmit={handleStartSubmit} className="flex flex-col gap-4">
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Start Reading</span>
+              </label>
+              <input
+                type="number"
+                value={startReading}
+                onChange={(e) => setStartReading(e.target.value)}
+                placeholder="Start Odometer Reading"
+                className="input input-bordered"
+                required
+              />
+            </div>
+            <button type="submit" className="btn btn-primary">
+              Save Start Reading
+            </button>
+          </form>
         </div>
+      </div>
 
-        <div className="form-control">
-          <label className="label">
-            <span className="label-text">End Reading</span>
-          </label>
-          <input
-            type="number"
-            value={endReading}
-            onChange={(e) => setEndReading(e.target.value)}
-            placeholder="End Odometer Reading"
-            className="input input-bordered"
-            required
-          />
+      {/* End Reading Form */}
+      <div className="card bg-base-100 shadow-xl">
+        <div className="card-body">
+          <SubHeading title="Enter End Reading" iconClass="fas fa-pencil-alt" />
+          <form onSubmit={handleEndSubmit} className="flex flex-col gap-4">
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">End Reading</span>
+              </label>
+              <input
+                type="number"
+                value={endReading}
+                onChange={(e) => setEndReading(e.target.value)}
+                placeholder="End Odometer Reading"
+                className="input input-bordered"
+                required
+              />
+            </div>
+            <button type="submit" className="btn btn-primary">
+              Save End Reading and Calculate Distance
+            </button>
+          </form>
         </div>
-
-        <button type="submit" className="btn btn-primary">
-          Submit
-        </button>
-      </form>
+      </div>
     </div>
   );
 };
