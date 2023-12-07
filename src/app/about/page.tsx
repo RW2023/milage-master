3cb0be2d1d560d06d99101b30bc2aaff15501429
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-12-06 22:25:44 -0500

    update about page content detail the why of the app and look to the future

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,5 +1,4 @@
 'use client';
-import Heading from '@/components/ui/Heading'; 
 import React from 'react';
 
 export default function About() {
@@ -7,95 +6,62 @@ export default function About() {
     <div className="bg-base-200 min-h-screen">
       <div className="container mx-auto p-6">
         {/* Main Heading */}
-        <Heading
-          title="About Us"
-          iconClass="fas fa-truck"
-        />
+        <h1 className="text-5xl font-bold text-center mb-6">
+          About Mileage Master
+        </h1>
 
-        {/* Our Journey Section */}
+        {/* Introduction Section */}
         <div className="card bg-base-100 shadow-xl mb-4">
           <div className="card-body">
             <h2 className="card-title text-4xl">
-              Our Journey: From Code to Inspiration
+              A Practical Solution for Drivers
             </h2>
             <p>
-              Welcome to Inspirational Voices, a homage to the profound wisdom
-              and enduring legacies of black thought leaders throughout history.
-              What began as a simple coding exercise in JavaScript, has
-              blossomed into a powerful educational tool and a source of daily
-              inspiration.
+              Mileage Master was born from a simple need - to eliminate the
+              hassle of using a pen and paper for recording hub odometer
+                readings during pre-trip inspections. It&apos;s a straightforward,
+              no-frills app designed by a truck driver, for truck drivers.
             </p>
           </div>
         </div>
 
-        {/* The Initial Spark Section */}
+        {/* The Core Idea Section */}
         <div className="card bg-base-100 shadow-xl mb-4">
           <div className="card-body">
-            <h2 className="card-title text-4xl">The Initial Spark</h2>
+            <h2 className="card-title text-4xl">The Core Idea</h2>
             <p>
-              As a budding developer, I set out to refine my skills in
-              JavaScript, particularly focusing on array methods. The idea was
-              straightforward - create a random quote generator. However, as I
-              delved deeper into the project, I realized that this could be more
-              than just a coding exercise. It could be a platform to amplify
-              voices that have shaped history and continue to inspire us today.
+              The core idea of Mileage Master is to make the daily routine of
+              recording mileage as easy as possible. It not only helps in
+              logging the start and end readings but also tracks the distance
+              covered, especially when pulling a trailer.
             </p>
           </div>
         </div>
 
-        {/* A Tribute to Thought Leaders Section */}
+        {/* Coding Practice and Problem Solving Section */}
         <div className="card bg-base-100 shadow-xl mb-4">
           <div className="card-body">
             <h2 className="card-title text-4xl">
-              A Tribute to Thought Leaders
+              Coding Practice Meets Problem Solving
             </h2>
             <p>
-              Thus, Inspirational Voices was born - a digital tribute to black
-              thought leaders. Each time you click the quote generator button,
-              not only does a random, impactful quote appear, but so does the
-              image of the leader who said it. This integration of visual and
-              textual elements aims to create a more immersive and meaningful
-              experience.
+              This app is also a personal journey in coding practice, blending
+              learning with solving real-world problems. It stands as a
+              testament to the power of technology in simplifying everyday
+              tasks.
             </p>
           </div>
         </div>
 
-        {/* More Than Just an App Section */}
-        <div className="card bg-base-100 shadow-xl mb-4">
-          <div className="card-body">
-            <h2 className="card-title text-4xl">More Than Just an App</h2>
-            <p>
-              This app is a blend of technology and history, designed to
-              educate, motivate, and resonate with its users. It&apos;s a
-              testament to how a simple practice in coding can evolve into
-              something impactful and significant.
-            </p>
-          </div>
-        </div>
-
-        {/* Our Commitment Section */}
-        <div className="card bg-base-100 shadow-xl mb-4">
-          <div className="card-body">
-            <h2 className="card-title text-4xl">Our Commitment</h2>
-            <p>
-              We are committed to continuously enriching our database with more
-              quotes and leaders, ensuring that every interaction with our app
-              is a unique and enlightening experience. Our goal is to inspire
-              our users through the powerful words of these leaders, fostering a
-              deeper understanding and appreciation of their contributions.
-            </p>
-          </div>
-        </div>
-
-        {/* Join Our Journey Section */}
+        {/* Looking Forward Section */}
         <div className="card bg-base-100 shadow-xl mb-6">
           <div className="card-body">
-            <h2 className="card-title text-4xl">Join Our Journey</h2>
+            <h2 className="card-title text-4xl">Looking Forward</h2>
             <p>
-              We invite you to explore, learn, and be inspired. Every quote you
-              discover here is a step towards understanding the profound impact
-              these leaders have had on the world. Thank you for being part of
-              this journey.
+              As Mileage Master evolves, the goal is to continue refining its
+                capabilities, enhancing its utility for all truck drivers. It&apos;s
+                more than just an app; it&apos;s a growing solution aimed at making a
+                driver&apos;s life on the road a bit easier.
             </p>
           </div>
         </div>
