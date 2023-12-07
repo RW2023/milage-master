2a41a7854e69b6c4f26759a51180bfab952f36de
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-12-07 06:51:05 -0500

    set content type to application json relieve error response of invalid json

--- src/pages/api/addReading.ts
+++ src/pages/api/addReading.ts
@@ -1,26 +1,19 @@
-// src/pages/api/addReading.ts
-
+// addReading.ts
 import type { NextApiRequest, NextApiResponse } from 'next';
-import dbConnect from '@/lib/mongodb'; 
-import Reading from '@/models/Reading'; 
+import dbConnect from '@/lib/mongodb';
+import Reading from '@/models/Reading';
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
-  // Connect to the database
   await dbConnect();
 
-  // Handle POST request
   if (req.method === 'POST') {
     try {
-      // Create a new reading from the request body
       const newReading = new Reading(req.body);
-
-      // Save the reading to the database
       const savedReading = await newReading.save();
 
-      // Send back the saved reading
+      res.setHeader('Content-Type', 'application/json');
       res.status(201).json(savedReading);
     } catch (error) {
-      // Error handling
       if (error instanceof Error) {
         res.status(400).json({ error: error.message });
       } else {
@@ -28,7 +21,6 @@ async function handler(req: NextApiRequest, res: NextApiResponse) {
       }
     }
   } else {
-    // Respond with method not allowed if not a POST request
     res.status(405).json({ error: 'Method Not Allowed' });
   }
 }
--- src/pages/api/getReading.ts
+++ src/pages/api/getReading.ts
@@ -1,6 +1,7 @@
+// getReading.ts
 import type { NextApiRequest, NextApiResponse } from 'next';
-import dbConnect from '@/lib/mongodb'; 
-import Reading from '@/models/Reading'; 
+import dbConnect from '@/lib/mongodb';
+import Reading from '@/models/Reading';
 
 export default async function handler(
   req: NextApiRequest,
@@ -10,19 +11,17 @@ export default async function handler(
 
   if (req.method === 'GET') {
     try {
-      const readings = await Reading.find({}); // Fetch all readings
+      const readings = await Reading.find({}).lean(); // Using .lean() for plain JavaScript objects
+      res.setHeader('Content-Type', 'application/json');
       res.status(200).json(readings);
     } catch (error) {
       if (error instanceof Error) {
-        // Properly handle the error if it's an instance of Error
         res.status(400).json({ error: error.message });
       } else {
-        // Generic error handling for unexpected errors
         res.status(500).json({ error: 'An unexpected error occurred' });
       }
     }
   } else {
-    // Handle any non-GET requests
-    res.status(405).end(`Method ${req.method} Not Allowed`);
+    res.status(405).json({ error: 'Method Not Allowed' });
   }
 }
