aacf8d4feab36679728991649b9e01c60fb11899
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2024-01-03 18:44:08 -0500

    Update page layout. change responsiveness to look different on different devices

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import OdometerForm from '@/components/OdemeterForm'
 
 export default function Home() {
   return (
-    <div className='flex flex-col justify-center items-center  text- font-bold'>
+    <div className='flex flex-col justify-center items-center font-bold md:w-3/4 lg:w-2/3 w-full mx-auto'>
       <OdometerForm />
     </div>
   )
